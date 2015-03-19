d4a7ec6448ed6786c5ae0e89b2b1b44aa437c5c5
VenAAX <venaax@moarcores.net>
2015-03-19 17:35:23 -0400

    Fixed bugs in GUI program. Added functionality to GUI program. All other programs are DEPRECIATED.

--- FSGui-Compiled-setup/PyLogParse.py
+++ FSGui-Compiled-setup/PyLogParse.py
@@ -1,3 +1,4 @@
+#FSlogparse.py with a GUI to make it a little less shit to use.
 import re
 import easygui as w
 import string
@@ -11,26 +12,23 @@ def logdecode(logop):
     logfin = logop
     logfin = logfin.splitlines(0)
     logfin = [value + "\n" for value in logfin]
-    print(logfin)
     valrem = re.findall(r'.*is offline.\n',logop) #remove online/offline messages
     logfin = remlist(logfin,valrem)
-    print (valrem)
     valrem = re.findall(r'.*is online.\n',logop) #Continued
     logfin = remlist(logfin,valrem)
+    valrem = re.findall('\[\d\d:\d\d\] DCS.*\n',logop) #remove DCS messages. Level up and OOC chat
     print(valrem)
-    valrem = re.findall('\[\S+\s+DCS.*\n',logop) #remove DCS messages. Level up and OOC chat
     logfin = remlist(logfin,valrem)
-    print(valrem)
-    valrem = re.findall(' Total Xp: .*\n',logop) #Remove total XP after XP add
+    valrem = re.findall('Total Xp: .*\n',logop) #Remove total XP after XP add
     logfin = remlist(logfin,valrem)
-    print(valrem)
     valrem = re.findall('\[\S+\s+ Second Life:.*\n',logop) #Remove music change, SL messages
     logfin = remlist(logfin,valrem)
+    valrem = re.findall('\[\d\d:\d\d\] TOX.*\n',logop) #Remove tip messages, lamp post crap, etc
     print(valrem)
-    #leaving timestamps in chat
-    print(logfin)
-    
-
+    logfin = remlist(logfin,valrem)
+    valrem = re.findall('\[\d\d:\d\d\] Toxic.*\n',logop) #Remove squid crap
+    print(valrem)
+    logfin = remlist(logfin,valrem)
     return logfin
 
 title = "Toxia Log NC helper"
@@ -38,9 +36,3 @@ msg = "Paste logs in here"
 logop = w.enterbox(msg,title)
 logfin = logdecode(logop)
 w.codebox("logs below","Cleaned Logs",logfin)
-
-
-
-
-
-
